38b75b0b7dc88a0f2fa4d104326084cd31ed319c
okryzhan <sashakryzh@gmail.com>
2019-10-22 12:41:42 +0300

    Initial Commit

---
+++ swifty-companion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swifty-companion
+//
+//  Created by Oleksandr KRYZHANOVSKYI on 10/22/19.
+//  Copyright © 2019 okryzhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
